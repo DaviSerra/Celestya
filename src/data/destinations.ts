491edd2b5b99ad8b4277afa6f3c773b0073026a7
davy <davyserra67@gmail.com>
2025-03-18 23:21:11 -0400

    feat: construção da home

---
+++ src/data/destinations.ts
@@ -0,0 +1,31 @@
+export  const destinations = [
+    {
+      name: "Moon",
+      description: "See Earth's only natural satellite up close.",
+      distance: "384.4K KM",
+      travelTime: "3 Days",
+      image: "moon.jpg",
+    },
+    {
+      name: "Mars",
+      description:
+        "Tackle Olympus Mons, the tallest planetary mountain in the solar system.",
+      distance: "225 MIL. KM",
+      travelTime: "9 Months",
+      image: "mars.jpg",
+    },
+    {
+      name: "Europa",
+      description: "Explore Jupiter's icy moon, with a hidden ocean beneath.",
+      distance: "628 MIL. KM",
+      travelTime: "6 Years",
+      image: "europa.jpg",
+    },
+    {
+      name: "Titan",
+      description: "Visit Saturn's largest moon, with its dense atmosphere.",
+      distance: "1.4 BIL. KM",
+      travelTime: "7 Years",
+      image: "titan.jpg",
+    },
+  ];
