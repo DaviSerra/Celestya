2ab5b848770e173bedf679484c3e531f6138d375
davy <davyserra67@gmail.com>
2025-03-27 22:09:49 -0400

    feat: ajustes na descrição das tecnologias

--- src/pages/Tecnologies/tecnologies.tsx
+++ src/pages/Tecnologies/tecnologies.tsx
@@ -7,10 +7,26 @@ const Tecnologies: React.FC = () => {
         <Breadcrumb title="Lançamento espacial 101" page="03" />
       </div>
 
-      <div className="flex flex-col justify-center container pt-20 mx-auto">
-        <h3>A terminologia</h3>
-        <h1>Veiculo espacial</h1>
+      <div className="flex justify-center pt-20 w-full gap-36">
+        <div className="flex flex-col items-center justify-center space-y-5">
+          <span className="flex justify-center items-center font-bellefair w-[80px] h-[80px] bg-white text-black rounded-full text-center text-4xl">1</span>
+          <span className="flex justify-center items-center font-bellefair w-[80px] h-[80px] bg-transparent border-[0.1px] border-white/25 text-white rounded-full text-center text-4xl">2</span>
+          <span className="flex justify-center items-center font-bellefair w-[80px] h-[80px] bg-transparent border-[0.1px] border-white/25 text-white rounded-full text-center text-4xl">3</span>
+        </div>
+
+
+        <div className="flex flex-col justify-start items-start max-w-3xl space-y-6">
+          <h3 className="text-white/50 font-bellefair text-4xl uppercase">A terminologia...</h3>
+          <h1 className="text-white text-5xl font-bellefair uppercase">Veículo de lançamento</h1>
+          <p>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+        </div>
       </div>
+
+
+
+
+
+
     </div>
   );
 };
