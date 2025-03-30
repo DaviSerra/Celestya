cbc1c5236c6e894905403d6861eae5bcbb3c7418
davy <davyserra67@gmail.com>
2025-03-30 16:36:34 -0400

    feat: deploy cloudflare

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Router />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Celestya/',
   plugins: [react(), tailwindcss()],
-  
+  base: "./", 
+  build: {
+    outDir: "dist",
+  },
 })
